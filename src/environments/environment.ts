0b94ea7c73c6a2ece62b817dae375ab01f69eeee
kiiaseilonen <kiia.seilonen@hotmail.com>
2022-09-21 12:24:42 +0300

    labwork 3 demo-app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '7d55a795cc544e6247433a4aa91571bd',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
